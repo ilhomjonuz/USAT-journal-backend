2cc9b6759bc6865cfd3df0f2afa4e75281180b40
RahimovIlhom <ilhomjonrahimov217@gmail.com>
2025-02-26 17:28:49 +0500

    mail text changed

--- apps/accounts/utils.py
+++ apps/accounts/utils.py
@@ -1,6 +1,7 @@
 from django.core.mail import send_mail
 from django.conf import settings
 import threading
+from django.utils.translation import gettext_lazy as _
 
 
 class EmailThread(threading.Thread):
@@ -8,9 +9,10 @@ class EmailThread(threading.Thread):
     Thread class for sending emails in the background
     """
 
-    def __init__(self, subject, message, recipient_list, from_email=None):
+    def __init__(self, subject, message, html_message, recipient_list, from_email=None):
         self.subject = subject
         self.message = message
+        self.html_message = html_message
         self.from_email = from_email or settings.DEFAULT_FROM_EMAIL
         self.recipient_list = recipient_list
         threading.Thread.__init__(self)
@@ -22,39 +24,54 @@ class EmailThread(threading.Thread):
             from_email=self.from_email,
             recipient_list=self.recipient_list,
             fail_silently=False,
+            html_message=self.html_message,  # HTML formatda jo‘natish
         )
 
 
-def send_email_async(subject, message, recipient_list, from_email=None):
+def send_email_async(subject, message, html_message, recipient_list, from_email=None):
     """
     Send email asynchronously using threading
     """
-    EmailThread(subject, message, recipient_list, from_email).start()
+    EmailThread(subject, message, html_message, recipient_list, from_email).start()
 
 
 def send_verification_email(email, verification_code):
     """
     Send verification code email asynchronously
     """
-    subject = 'Email Verification Code'
-    message = f'Your verification code is: {verification_code}'
+    subject = _('Email Verification Code')  # Tarjima ishlaydi
 
-    send_email_async(
-        subject=subject,
-        message=message,
-        recipient_list=[email]
-    )
+    message = _(
+        "Your verification code is: {code}\n\n"
+        "Do not share this code with anyone for security reasons."
+    ).format(code=verification_code)
+
+    html_message = f"""
+        <p>{_('Your verification code is:')}</p>
+        <h2 style="color: blue;">{verification_code}</h2>
+        <p><strong>{_('Please do not share this code with anyone!')}</strong></p>
+        <p>{_('If you did not request this, you can ignore this email.')}</p>
+    """
+
+    send_email_async(subject, message, html_message, [email])
 
 
 def send_password_reset_email(email, verification_code):
     """
     Send password reset code email asynchronously
     """
-    subject = 'Password Reset Code'
-    message = f'Your password reset code is: {verification_code}'
+    subject = _('Password Reset Code')
 
-    send_email_async(
-        subject=subject,
-        message=message,
-        recipient_list=[email]
-    )
+    message = _(
+        "Your password reset code is: {code}\n\n"
+        "Do not share this code with anyone for security reasons."
+    ).format(code=verification_code)
+
+    html_message = f"""
+        <p>{_('Your password reset code is:')}</p>
+        <h2 style="color: blue;">{verification_code}</h2>
+        <p><strong>{_('Please do not share this code with anyone!')}</strong></p>
+        <p>{_('If you did not request this, you can ignore this email.')}</p>
+    """
+
+    send_email_async(subject, message, html_message, [email])
