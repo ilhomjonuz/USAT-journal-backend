9e5dfc029108c02ed505f83186f3ff7b4e316421
RahimovIlhom <ilhomjonrahimov217@gmail.com>
2025-02-28 17:29:03 +0500

    locale messages prepare for change

--- apps/accounts/views/api_auth_views.py
+++ apps/accounts/views/api_auth_views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
 from rest_framework import status, generics, permissions
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.exceptions import TokenError
